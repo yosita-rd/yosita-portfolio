4ceb01718a44cc681a3c41f6fefcdb1439965361
popo <yosita.rd@gmail.com>
2024-12-20 12:29:40 +0700

    add project data picture and file

---
+++ public/data/project.tsx
@@ -0,0 +1,74 @@
+const Project = [
+  {
+    title: 'FloraMate',
+    date: 'Nov 2024',
+    org: 'University Assignment',
+    desc: 'Web application to monitor and maintain plants by providing real-time updates on images, temperature, soil and air humidity, and light levels.',
+    keyResponsibilities:
+      'UX/UI design, Implemented responsive frontend development.',
+    tags: ['frontend', 'responsive', 'UX/UI'],
+    tools: 'Next.js, React, Tailwind CSS, TypeScript, REST API, Firebase',
+    toolsIcon: ['nextjs', 'react', 'tailwind', 'typescript', 'firebase'],
+    link: '',
+    type: 'Web Dev',
+    image: '/projects/floramate.png',
+  },
+  {
+    title: 'Friday Activity System',
+    date: 'Nov 2024 - Present',
+    org: 'Chula Engineering',
+    desc: 'Website for Chula Engineering’s CEDT to facilitate knowledge-sharing workshops and real-world company collaborations. Activities occur every Friday.',
+    keyResponsibilities:
+      'Contributed to UX/UI design, Implemented responsive frontend development.',
+    tags: ['frontend', 'responsive', 'UX/UI'],
+    tools: 'Next.js, React, Tailwind CSS, TypeScript, REST API',
+    toolsIcon: ['nextjs', 'react', 'tailwind', 'typescript'],
+    link: '',
+    type: 'Web Dev',
+    image: '/projects/friday_activity_system.png',
+  },
+  {
+    title: 'Mindlink System',
+    date: 'Aug 2024 - Present',
+    org: 'MindLink',
+    desc: 'System to manage and maintain organizational tasks for tutors, including class management, payment processing, and dashboards.',
+    keyResponsibilities:
+      'Assisted in UX/UI design, Implemented responsive frontend development.',
+    tags: ['frontend', 'responsive', 'UX/UI'],
+    tools: 'Next.js, React, Tailwind CSS, TypeScript, REST API',
+    toolsIcon: ['nextjs', 'react', 'tailwind', 'typescript'],
+    link: '',
+    type: 'Web Dev',
+    image: '/projects/mindlink_system.png',
+  },
+  {
+    title: 'A2K',
+    date: 'Nov 2024 - Dec 2024',
+    org: 'University Assignment',
+    desc: 'A data science project utilizing data from the Chula University academic resource office to train models, extract keywords, and visualize results.',
+    keyResponsibilities:
+      'UX/UI design, Implemented frontend, Created data visualizations, Contributed to project proposal.',
+    tags: ['data-visualization', 'frontend', 'UX/UI'],
+    tools: 'Next.js, React, Tailwind CSS, Plotly.js, D3.js, JavaScript',
+    toolsIcon: ['nextjs', 'react', 'tailwind', 'plotly', 'd3'],
+    link: '',
+    type: 'Web Dev',
+    image: '/projects/a2k.png',
+  },
+  {
+    title: 'Net Down, Sprout Up',
+    date: 'Feb 2024 - Apr 2024',
+    org: 'University Assignment',
+    desc: 'Farming simulation game where players cultivate, plant, harvest, cook, and manage energy and inventory systems while advancing the storyline.',
+    keyResponsibilities:
+      'Designed features, Created gameplay mechanics, Developed interface.',
+    tags: ['game-design', 'gameplay', 'UI'],
+    tools: 'Java, JavaFX',
+    toolsIcon: ['java', 'javafx'],
+    link: '',
+    type: 'Game',
+    image: '/projects/net_down_sprout_up.png',
+  },
+];
+
+export default Project;
